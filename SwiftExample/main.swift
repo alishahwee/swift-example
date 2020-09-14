f769b4acd6d8e00d78289ed0bbe151bb54d1039a
alishahwee <alisha.hwee@outlook.com>
2020-09-14 18:20:28 -0500

    Initial Commit

---
+++ SwiftExample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftExample
+//
+//  Created by Alisha Hwee on 9/14/20.
+//  Copyright © 2020 Alisha Hwee. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
