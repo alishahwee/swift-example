e1e848260629af80ee4f6eeb1d0d9de76c3e5f08
alishahwee <alisha.hwee@outlook.com>
2020-09-14 19:01:04 -0500

    Finish tutorial

---
+++ SwiftExample/README.md
@@ -0,0 +1,3 @@
+#  swift-example
+
+Using CS50 track video lesson 1, here lies an example of using Swift code in the Xcode IDE for the first time. Woot!
--- SwiftExample/main.swift
+++ SwiftExample/main.swift
@@ -8,5 +8,30 @@
 
 import Foundation
 
-print("Hello, World!")
+class Track {
+    let name: String
+    let instructor: String
+    
+    init(name: String, instructor: String) {
+        self.name = name
+        self.instructor = instructor
+    }
+}
 
+let tracks = [
+    Track(name: "Mobile", instructor: "Tommy"),
+    Track(name: "Web", instructor: "Brian"),
+    Track(name: "Games", instructor: "Colton")
+]
+
+let students = ["Harry", "Ron", "Hermione"]
+
+var assignments: [String : Track] = [:]
+for student in students {
+    let track = Int.random(in: 0 ..< tracks.count)
+    assignments[student] = tracks[track]
+}
+
+for (student, track) in assignments {
+    print("\(student) got \(track.name) with \(track.instructor)")
+}
